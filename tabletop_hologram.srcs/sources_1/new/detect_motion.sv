54c3572bd06be6e3915ca0189a72ca27a3c1d1b8
Jeremy McCulloch <jeremymc@mit.edu>
2019-12-11 09:53:26 -0500

    cleaned up code

--- tabletop_hologram.srcs/sources_1/new/detect_motion.sv
+++ tabletop_hologram.srcs/sources_1/new/detect_motion.sv
@@ -1,25 +1,6 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/03/2019 01:46:41 PM
-// Design Name: 
-// Module Name: detect_motion
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
-
+// Detects motion of hand (previously a saber)
 module detect_motion(
     input clk_in,
     input rst_in,
@@ -28,20 +9,28 @@ module detect_motion(
     input saber_detected,
     output logic saber_moving
     );
-    parameter CYCLES_PER_SAMPLE = 20000000; // 200 ms
-    parameter PIXEL_THRESHOLD = 12'd25; // About 1 foot at chest height
+    
+    parameter CYCLES_PER_SAMPLE = 20000000; // 200 ms, how frequently we sample centroid
+    parameter PIXEL_THRESHOLD = 12'd25; // About 1 foot at chest height, how far hand must move in 200 ms to count as motion
+    
     
     logic [31:0] count;
+    
+    // Keep track of last saber detected / centroid values
     logic last_saber_detected;
     logic [10:0] last_centroid_x;
     logic [9:0] last_centroid_y;
+    
+    // Calculate distance moved since last time
     logic [10:0] delta_centroid_x;
     logic [9:0] delta_centroid_y;
     logic [19:0] dist_sq;
     
+    // Pipelined signals (since calculating distance takes multiple clock cycles)
     logic [10:0] lag_centroid_x;
     logic [9:0] lag_centroid_y;
     logic lag_saber_detected;
+    
     pipeline #(.N_BITS(11), .N_REGISTERS(2)) pipeline_x(
         .clk_in(clk_in), .rst_in(rst_in),
         .data_in(centroid_x), .data_out(lag_centroid_x)
@@ -63,13 +52,16 @@ module detect_motion(
             last_centroid_y <= 10'b0;
             saber_moving <= 1'b0;
         end else if (count == CYCLES_PER_SAMPLE - 3) begin
+            // On third to last  clock cycles calculate displacement
             count <= count + 32'b1;
             delta_centroid_x <= (centroid_x > last_centroid_x) ? (centroid_x - last_centroid_x) : (last_centroid_x - centroid_x);
             delta_centroid_y <= (centroid_y > last_centroid_y) ? (centroid_y - last_centroid_y) : (last_centroid_y - centroid_y);
         end else if (count == CYCLES_PER_SAMPLE - 2) begin
+            // On second to last clock cycle calculate distance squared
             count <= count + 32'b1;
             dist_sq <= (delta_centroid_x * delta_centroid_x + delta_centroid_y * delta_centroid_y);
         end else if (count == CYCLES_PER_SAMPLE - 1) begin
+            // On last clock cycle calculate whether it moved enough
             count <= 32'b0;
             last_saber_detected <= lag_saber_detected;
             last_centroid_x <= lag_centroid_x;
--- tabletop_hologram.srcs/sources_1/new/projection.sv
+++ tabletop_hologram.srcs/sources_1/new/projection.sv
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 
-module projection_with_height(
+module projection(
     input clk_in,
     input rst_in, 
     input signed [8:0][11:0] vertices_in,
@@ -9,22 +9,23 @@ module projection_with_height(
     output logic signed [8:0][11:0] vertices_out,
     output logic finished_out
 );
+    // Project each vertex separately
     parameter DIVIDER_LATENCY = 21;
-    project_vertex_with_height vertex1(
+    project_vertex vertex1(
         .clk_in(clk_in), .rst_in(rst_in),
         .vertex_in(vertices_in[8:6]),
         .user_in(user_in),
         .new_data_in(new_data_in),
         .vertex_out(vertices_out[8:6])
     );
-    project_vertex_with_height vertex2(
+    project_vertex vertex2(
         .clk_in(clk_in), .rst_in(rst_in),
         .vertex_in(vertices_in[5:3]),
         .user_in(user_in),
         .new_data_in(new_data_in),
         .vertex_out(vertices_out[5:3])
     );
-    project_vertex_with_height vertex3(
+    project_vertex vertex3(
         .clk_in(clk_in), .rst_in(rst_in),
         .vertex_in(vertices_in[2:0]),
         .user_in(user_in),
@@ -33,14 +34,14 @@ module projection_with_height(
     );
     
     pipeline #(.N_BITS(1), .N_REGISTERS(3 + DIVIDER_LATENCY)) pipeline_finished (
-    .clk_in(clk_in), .rst_in(rst_in),
-    .data_in(new_data_in),
-    .data_out(finished_out)
+        .clk_in(clk_in), .rst_in(rst_in),
+        .data_in(new_data_in),
+        .data_out(finished_out)
     );
     
 endmodule
 
-module project_vertex_with_height(
+module project_vertex(
     input clk_in,
     input rst_in, 
     input signed [2:0][11:0] vertex_in,
@@ -51,11 +52,14 @@ module project_vertex_with_height(
 
     parameter SCREEN_WIDTH = 400;
     parameter SCREEN_HEIGHT = 400;
+    
+    // Registers to store inputs
     logic signed [11:0] vx, vy, vz, ux, uy, uz;
+    
+    // Divider inputs / outputs
     logic signed [23:0] numerator_x, numerator_y;
     logic signed [15:0] denominator;
     logic signed [23:0] divider_out_x, divider_out_y;
-    
     logic divider_valid_in, divider_x_valid_out, divider_y_valid_out;
     
     pipeline #(.N_BITS(1), .N_REGISTERS(2)) pipeline_divider_valid(
@@ -97,6 +101,7 @@ module project_vertex_with_height(
             vertex_out <= 0;
             
         end else if (new_data_in) begin
+            // Save inputs in local register on new_data_in
             vx <= $signed(vertex_in[2]);
             vy <= $signed(vertex_in[1]);
             vz <= $signed(vertex_in[0]);
@@ -104,13 +109,14 @@ module project_vertex_with_height(
             uy <= $signed(user_in[1]);
             uz <= $signed(user_in[0]);
         end else begin
+            // vertex_out_x = (vz * ux - uz * vx) / (vz - uz)
+            // vertex_out_y = (vz * uy - uz * vy) / (vz - uz)
+            // vertex_out_z = vz
             numerator_x <= vz * ux - uz * vx;
             numerator_y <= vz * uy - uz * vy;
             denominator <= vz - uz;
             vertex_out[2] <= divider_x_valid_out ? (divider_out_x + SCREEN_WIDTH / 2) : vertex_out[2];
             vertex_out[1] <= divider_y_valid_out ? (divider_out_y + SCREEN_HEIGHT / 2) : vertex_out[1];
-//            vertex_out[2] <= divider_x_valid_out ? divider_out_x : vertex_out[2];
-//            vertex_out[1] <= divider_y_valid_out ? divider_out_y : vertex_out[1];
             vertex_out[0] <= vz;
 
         end
